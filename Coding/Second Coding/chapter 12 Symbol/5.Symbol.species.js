43b138e7a6132aa74907350b3ffccb3962a3de14
StellaYangFan <1062878468@qq.com>
2019-08-26 12:45:56 +0800

    docs update Symbol.md

--- Coding/Second Coding/chapter 12 Symbol/5.Symbol.species.js
+++ Coding/Second Coding/chapter 12 Symbol/5.Symbol.species.js
@@ -1,7 +1,23 @@
-class ArrayModel extends Array {}
+class ArrayModel extends Array {
+  // static get [Symbol.species] () {
+  //   return this;
+  // }
+}
 
 let a = new ArrayModel(1,2,3);
 let b = a.map( n => n);
 let c = a.map( n => n * 2);
-b instanceof ArrayModel;
-c instanceof ArrayModel;
+b instanceof ArrayModel;    // true
+c instanceof ArrayModel;    // true
+
+// set [Symbol.species]() { return this; }
+class ArrayModel extends Array {
+  static get [Symbol.species] () {
+    return Array;
+  }
+}
+let a = new ArrayModel(1,2,3);
+let b = a.map( n => n);
+let c = a.map( n => n * 2);
+console.log(b instanceof ArrayModel);    // false
+console.log(c instanceof Array);         // true 
