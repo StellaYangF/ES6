b8d359d634eac01cacbdd754c7de953e3d575b70
StellaYangFan <1062878468@qq.com>
2019-09-02 15:39:17 +0800

    this problem

--- Coding/Second Coding/chapter 14 Proxy/16.this.problem.js
+++ Coding/Second Coding/chapter 14 Proxy/16.this.problem.js
@@ -21,3 +21,28 @@ class Person {
   }
 }
 
+const jane = new Person('Jane');
+jane.name // 'Jane
+const p = new Proxy(jane, {});
+p.name // undefined
+
+// 2) example 2: 
+// 有些原生对象的内部属性，只有通过正确的this才能拿到，所以 Proxy 也无法代理这些原生对象的属性。
+const target = new Date();
+const handler = {};
+const p = new Proxy(target, handler);
+p.getDate();
+// Uncaught TypeError: this is not a Date object.
+
+// 3) example 3: 
+const target = new Date('2019-09-02');
+const hanlder = {
+  get (target, prop) {
+    if (prop === 'getDate') {
+      return target.getDate.bind(target);
+    }
+    return Reflect.get(target, prop);
+  }
+};
+const p = new Proxy(target, hanlder);
+p.getDate();  // 2
