8431bb1151ff5dfc94f1bbeb6a74cba9a46bbb05
StellaYangFan <1062878468@qq.com>
2019-09-02 09:30:34 +0800

    proxy instance method: apply

--- Coding/Second Coding/chapter 14 Proxy/3.set.js
+++ Coding/Second Coding/chapter 14 Proxy/3.set.js
@@ -34,6 +34,30 @@ const obj = {};
 Object.setPrototypeOf(obj, proxy);
 obj.foo = 'bar';
 obj.foo === obj;  // true
-/************************************************************************/
-/************************************************************************/
-/************************************************************************/
+
+/******************************属性不可配置且不可写时,set代码不生效******************************************/
+const obj = {};
+Object.defineProperty(obj,'foo', {
+  value: 'bar',
+  writable: false,
+})
+const handler = {
+  set (obj, prop, value, receiver) {
+    obj[prop] = 'baz';
+  }
+}
+const proxy = new Proxy(obj, handler);
+proxy.foo = 'baz';
+proxy.foo   // bar
+/*******************************严格模式下，不返回true报错*****************************************/
+'use strict';
+const handler = {
+  set (target, prop, value, receiver) {
+    target[prop] = receiver;
+    // 无论是否有下面这行，都会报错，或返回值为undefined
+    return false;
+  }
+}
+const proxy = new Proxy({},handler);
+proxy.foo = 'bar';
+// Uncaught TypeError: 'set' on proxy: trap returned falsish for property 'foo'
