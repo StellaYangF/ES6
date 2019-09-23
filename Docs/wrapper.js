081cde11be18feec3eb406ba2c8808bb0427926e
StellaYangFan <1062878468@qq.com>
2019-09-23 17:56:50 +0800

    generator

--- Docs/wrapper.js
+++ Docs/wrapper.js
@@ -1,8 +1,12 @@
+// Generator 函数如果不用wrapper先包一层，是无法第一次调用next方法，就输入参数的。
+
 function wrapper(generatorFunction) {
+  // 传入generator函数，返回一个函数接收参数
   return function (...args) {
     let generatorObject = generatorFunction(...args);
     // 直接调用一次  输入参数给yield返回值用
     generatorObject.next();
+    // 返回生成器函数
     return generatorObject;
   }
 }
@@ -12,3 +16,4 @@ const warpped = wrapper(function* () {
   return "DONE";
 })
 warpped().next("hello");
+
